ea51febd24b5ba9758e3393e66c1fb59cf63bf84
stang91 <s.tang32191@gmail.com>
2022-08-03 16:31:03 -0700

    base on files

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));//add current date to jombotron
+window.setInterval(function () {
+    $('#currentDay').html(moment().format('LLLL'));
+}, 1000);
+
+
+for(var i=1; i<12; i++){
+    var r=i-1;
+    var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
+    $('#addClass').addClass('time'+[i]).text([i]+' AM').removeClass('time'+[r]);
+    $('.container').append(moreInput);
+}
+if ( i=12){
+    var r=i-1;
+    var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
+    $('#addClass').addClass('time'+[i]).text([i]+' PM').removeClass('time'+[r]);
+    $('.container').append(moreInput);
+}
+for(var i=13; i<24; i++){
+    var r=i-1;
+    var w=i-12;
+    var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
+    $('#addClass').addClass('time'+[i]).text([w]+' PM').removeClass('time'+[r]);
+    $('.container').append(moreInput);
+}
