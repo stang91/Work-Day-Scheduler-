57ff774b500798d4cdecdef9263a9e045d4621a9
stang91 <s.tang32191@gmail.com>
2022-08-03 18:13:42 -0700

    base on files

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,19 @@ for(var i=1; i<12; i++){
     var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
     $('#addClass').addClass('time'+[i]).text([i]+' AM').removeClass('time'+[r]);
     $('.container').append(moreInput);
-}
+};
 if ( i=12){
     var r=i-1;
     var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
     $('#addClass').addClass('time'+[i]).text([i]+' PM').removeClass('time'+[r]);
     $('.container').append(moreInput);
-}
+};
 for(var i=13; i<24; i++){
     var r=i-1;
     var w=i-12;
     var moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
     $('#addClass').addClass('time'+[i]).text([w]+' PM').removeClass('time'+[r]);
+    $('textarea').attr('id','time-'+[i]);
     $('.container').append(moreInput);
-}
+};
+
