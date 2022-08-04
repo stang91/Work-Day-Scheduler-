f287b1bd789a96f27b3ab4cf30edb11966279c6b
stang91 <s.tang32191@gmail.com>
2022-08-04 13:11:37 -0700

    base on files

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,42 @@
 window.setInterval(function () {
     $('#currentDay').html(moment().format('LLLL'));
 }, 1000);
-
-
-for(var i=1; i<12; i++){
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
-    $('#addClass').text([i]+' AM');
+// duplicate time block
+let n =5;
+let b =23;
+for(var i=n; i<12; i++){
+    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
+    $('#addClass').text([i]+'AM');
     $('.container').append(moreInput);
 };
-if ( i=12){
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
-    $('#addClass').text([i]+' PM')
+if (i=12){
+    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
+    $('#addClass').text([i]+'PM')
     $('.container').append(moreInput);
 };
-for(var i=13; i<24; i++){
+for(var i=12; i<b; i++){
     let w=i-12;
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
-    $('#addClass').text([w]+' PM');
+    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
+    $('#addClass').text([w]+'PM');
     $('.container').append(moreInput);
 };
-if(i=24){
+if(i=b){
     let w=i-12;
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-hours0 custom-clone');
-    $('#addClass').text([w]+' AM');
+    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
+    $('#addClass').text([w]+'AM');
     $('.container').append(moreInput);
 };
 
-$('.custom-hours1').remove();
+$('.custom-hours'+[n]).remove();
 
 //https://stackoverflow.com/questions/25680333/how-to-add-ids-to-tr-using-loop-in-jquery
-$('.time-block').each(function(index){
-    $(this).prop('id','time'+(index+1));
- });
+$('.description').each(function(index){
+    $(this).prop('id','time'+(index+(n+1)));
+});
 
-let now=moment().hour()
- $('.time-block').each(function () {
+let now=moment().hour();
+ $('.description').each(function () {
     let blockTime=parseInt($(this).attr('id').split('time')[1]);
-
     // To check the time and add the classes for background indicators
     if (now < blockTime) {
         $(this).addClass('future');
@@ -45,16 +45,17 @@ let now=moment().hour()
     } else {
         $(this).addClass('past');
     }
+});
+
+
+$('.saveBtn').click(function(){
     
 });
 
-$('.saveBtn').click(function(){
-    let savedDataText=$(this).sibbling('.time-block').val();
-    let time=$(this).sibbling('.description').text();
-    let savedObj={
-        savedTime: time,
-        textData: savedDataText
-    }
-    localStorage.setItem('savedObj',JSON.stringify(savedObj));
-});
+$('.btnClear').click(function(){
 
+    $('.description').each(function(){
+        $(this).empty();
+    });
+    localStorage.clear();
+});
