0f4015923bacc2126b305111abab637e7535c186
stang91 <s.tang32191@gmail.com>
2022-08-05 01:20:15 -0700

    base on files

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,43 @@
+
+
 window.setInterval(function () {
     $('#currentDay').html(moment().format('LLLL'));
 }, 1000);
 // duplicate time block
-let n =5;
-let b =23;
-for(var i=n; i<12; i++){
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
-    $('#addClass').text([i]+'AM');
-    $('.container').append(moreInput);
-};
-if (i=12){
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
-    $('#addClass').text([i]+'PM')
-    $('.container').append(moreInput);
-};
-for(var i=12; i<b; i++){
-    let w=i-12;
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
-    $('#addClass').text([w]+'PM');
-    $('.container').append(moreInput);
-};
-if(i=b){
-    let w=i-12;
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+[i]).removeClass('custom-clone');
-    $('#addClass').text([w]+'AM');
-    $('.container').append(moreInput);
-};
+let n =1;
+let b =25;
+
+for(var i=n; i<b; i++){  
+    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+i).removeClass('custom-clone');
+    if (i%12===0){
+        $('#addClass').text('12PM')
+    }
+    else if(i%24===0){
+        $('#addClass').text('12AM')
+    }
+    else if(i%12!=i%24){
+    let j=i%12;
+    $('#addClass').text(j+'PM')
+    }
+    else if(i%12>0 && i%24){
+        let j=i%12;
+        $('#addClass').text(j+'AM')
+    }
+    
+$('.container').append(moreInput);
 
 $('.custom-hours'+[n]).remove();
+};
 
 //https://stackoverflow.com/questions/25680333/how-to-add-ids-to-tr-using-loop-in-jquery
 $('.description').each(function(index){
-    $(this).prop('id','time'+(index+(n+1)));
+    $(this).prop('id','content'+(index+(n-1)));
+    $(this).siblings('.hour').prop('id','time'+(index+(n-1)));
 });
 
 let now=moment().hour();
  $('.description').each(function () {
-    let blockTime=parseInt($(this).attr('id').split('time')[1]);
+    let blockTime=parseInt($(this).attr('id').split('content')[1]);
     // To check the time and add the classes for background indicators
     if (now < blockTime) {
         $(this).addClass('future');
