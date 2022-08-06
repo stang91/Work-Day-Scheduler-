bfb19a171bd39b37648c9d07694cab7faa06742b
stang91 <s.tang32191@gmail.com>
2022-08-05 22:06:45 -0700

    base on file

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,46 @@
-
-
 window.setInterval(function () {
     $('#currentDay').html(moment().format('LLLL'));
 }, 1000);
 // duplicate time block
-let n =1;
+let n =5;
 let b =25;
-
-for(var i=n; i<b; i++){  
-    let moreInput= $('.custom-clone').clone().addClass('custom-hours'+i).removeClass('custom-clone');
+//duplicate time block and insert class and text
+for(var i=n; i<(b+2); i++){  
+    //clone time block and adding and removing class
+    let cloneTimeBlock= $('.custom-clone').clone().addClass('custom-hours'+i).removeClass('custom-clone');
+    //reasoning and logic for adding text to time indicator
     if (i%12===0){
-        $('#addClass').text('12PM')
+        $('#addClass').text('12PM');
     }
     else if(i%24===0){
-        $('#addClass').text('12AM')
+        $('#addClass').text('12AM');
     }
     else if(i%12!=i%24){
     let j=i%12;
-    $('#addClass').text(j+'PM')
+    $('#addClass').text(j+'PM');
     }
-    else if(i%12>0 && i%24){
+    else {
         let j=i%12;
-        $('#addClass').text(j+'AM')
+        $('#addClass').text(j+'AM');
     }
-    
-$('.container').append(moreInput);
-
-$('.custom-hours'+n).remove();
+    // append clone to div container
+    $('.container').append(cloneTimeBlock);
+    //removes original empty clone    
+    $('.custom-hours'+n).remove();
 };
 
 //https://stackoverflow.com/questions/25680333/how-to-add-ids-to-tr-using-loop-in-jquery
+//add id attribute to description class and hour class
 $('.description').each(function(index){
+    //add id attr to content[i]
     $(this).prop('id','content'+(index+(n-1)));
+    //replace #addClass id attribute to time[i]
     $(this).siblings('.hour').prop('id','time'+(index+(n-1)));
 });
+//remove cloning time block
+$('.custom-clone').remove();
 
+//change the description classes to background indicators
 let now=moment().hour();
  $('.description').each(function () {
     let blockTime=parseInt($(this).attr('id').split('content')[1]);
@@ -48,15 +54,25 @@ let now=moment().hour();
     }
 });
 
+//https://stackoverflow.com/questions/42963091/how-to-save-the-value-of-textarea-to-localstorage-then-display-it-in-the-same-te
+// create a loop for localstorage
 
-$('.saveBtn').click(function(){
-    
-});
+for (var i = n; i < (b+2); i++) {
+    //get stored content 
+  $("#content"+i).val(localStorage.getItem("content"+i));
+}
+
+$('.saveBtn').click(function saveNotes() {
+    $(this).siblings(".description").removeClass("notSaved").addClass("Saved");
+    // Save data to localstorage
+    for (var i = n; i < (b+1); i++) {
+        if($('.description').val()!==null){
+            localStorage.setItem("content"+ i, $("#content"+i).val());
+        }
+    }
+  });
 
 $('.btnClear').click(function(){
-
-    $('.description').each(function(){
-        $(this).empty();
-    });
+    $('.description').val('');
     localStorage.clear();
 });
